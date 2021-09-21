1f137d73410dd13593dc56fb1f0b69dcb33b7eb7
nluecap <nluecap@gmail.com>
2021-09-21 23:49:41 +0200

    updated app to v5 with webhook

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 4 of the app.' + '\n';
+    response = 'This is version 5 with webhook of the app.' + '\n';
 
     //send the response to the client
     res.send(response);
@@ -15,3 +15,4 @@ app.get('/', function (req, res) {
 app.listen(8080, function () {
   console.log('Server listening on port 8080...');
 });
+updated app to v2
